f3ec189927de5b578cf0dab863db6ba4228187bd
keita_momiyama <93181795+keita034@users.noreply.github.com>
2023-06-28 00:59:09 +0900

    初期化

--- Scene/GameScene.h
+++ Scene/GameScene.h
@@ -23,55 +23,6 @@ class GameScene : public BaseScene
 {
 private:
 
-	enum class DemoIndex
-	{
-		SKINNING_ANIMATION,
-		PHONG_SHADER,
-		TOON_SHADER,
-		POSTEFFECT,
-	};
-
-	std::vector<const char*> demoList{{"SKINNING_ANIMATION"}, { "PHONG_SHADER" },  {"TOON_SHADER"}, {"POSTEFFECT"} };
-	int32_t demoIndex;
-
-	std::unique_ptr<Light> light;
-	AliceMathF::Vector4 lightColor = { 1, 1, 1, 1 };
-
-	std::unique_ptr<GameCamera> camera;
-
-	std::unique_ptr<AliceModel>skinningModel;
-	uint32_t skinningModelHandle;
-	Transform skinningModelTransform;
-	float modelAnimationFrame = 0.0f;
-
-	std::unique_ptr<AliceMotionData> modelAnimation;
-	uint32_t modelAnimationHandle;
-
-	std::unique_ptr<AliceModel>sphereModel;
-	std::unique_ptr<AliceToonModel>sphereToonModel;
-	uint32_t sphereModelHandle;
-	uint32_t sphereToonModelHandle;
-	Transform sphereModelTransform;
-
-
-	std::unique_ptr<AliceModel>posteffectModel;
-	uint32_t posteffectModelHandle;
-	Transform posteffectModelTransform;
-
-	std::array<bool, 2>stripedPosteffect;
-	std::array<bool, 2>gaussianBlurPosteffect;
-	std::array<bool, 2>oilPaintingPostEffect;
-	std::array<bool, 2>chromaticAberrationPostEffect;
-	std::array<bool, 2>vignettePostEffect;
-
-	std::array<bool, 2>bloomPostEffect;
-	std::array<bool, 2>sandStormPostEffect;
-	std::array<bool, 2>crtPostEffect;
-	std::array<bool, 2>monochromePostEffect;
-	std::array<bool, 2>frostedGlassPostEffect;
-
-	std::array<bool, 2>mosaicPostEffect;
-	std::array<bool, 2>distortionPostEffect;
 
 public:
 
@@ -98,7 +49,6 @@ public:
 	/// </summary>
 	void Finalize() override;
 
-	void PostEffectGui();
 
 private:
 	//コピーコンストラクタ・代入演算子削除
